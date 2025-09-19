fea449f941a282054fa8ef7fed06dc43f0c6d59d
Abhinay Bari <abhinaybari794@gmail.com>
2025-09-19 14:30:01 +0530

    Javascript Using Spread-Operators

--- Function.js
+++ Function.js
@@ -1,5 +1,9 @@
-function text(A , B, C){
-    return (A + B + C)
-}
-let result = text(5, 6, 10)
-console.log(result);
+const text1 = [1 , 2, 3 , 5, 6 , 7 , 8, 9, 10]
+
+const text2 = [11, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ];
+
+const mergeText =[...text1,...text2]
+
+const showResult = (mergeText);
+
+console.log(showResult);
